156873dff16c4417c9eb83ca1f4fb259b1d280c7
aarav-2213 <naniangels2213@gmail.com>
2025-04-24 18:16:41 +1000

    Add files via upload

---
+++ act.1.py
@@ -0,0 +1,19 @@
+a=22
+b=12
+c=19
+d=11
+
+y=a*b/(c+d)
+print("answer:",y)
+
+
+
+age=12.25
+
+n=str(input("enter your name"))
+
+if n=="saivaish"  or n=="aarav" and a>15:
+    print("allowed")
+
+else:
+    print("not allowed")
---
+++ act.2.py
@@ -0,0 +1,8 @@
+n1=int(input("enter number 1:"))
+n2=int(input("enter number 2:"))
+
+if n1%n2==0:
+    print(str(n1)+ "is divisible by "+ str(n2))
+
+else:
+        print(str(n1)+ "is not divisible by "+ str(n2))
